8bc0c17e8a4844e57b4af745edbd2e6b712a3ee2
LoganUNWSP <lkholmstrom@students.unwsp.edu>
2025-01-31 17:01:18 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,6 @@
-def personal_information():
-    print('...')
-
-
-# Line which calls the above function.
-personal_information()
+def personal_info(info):
+    print(info)
+personal_info("Logan Holmstrom")
+personal_info("2874 Pyhton Ave, Saint Cloud, Minnesota, 56303")
+personal_info("320-827-2754")
+personal_info("PSEO")
